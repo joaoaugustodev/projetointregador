f0c67a04754542e87c4dead574359faaee6e2eb1
João Luís <joaoluis@ibaro.com.br>
2017-09-28 14:43:22 -0300

    algumas mudanças

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<?php 
-
-/**
- * {Author} João Augusto, Andrei, Bruno Fagundes, Julho
- * {description} Aqui apenas verificamos as urls para a validação das rotas
-*/
-
-include_once("./routes/routes.class.php");
-
-$pages = new Routes();
+<?php
