a5bf5618e3a9bcebb1e3fc1bddc3658bf9137b81
brunofagundesp <bruno_fagundesp@hotmail.com>
2017-10-04 21:12:44 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,77 +1,26 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['user'])) {
+        header('Location: /projetointregador/');
+        exit();
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!-- Compiled and minified CSS -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.0/css/materialize.min.css">
-      <link rel="stylesheet" href="http://github.hubspot.com/odometer/themes/odometer-theme-default.css" />
-      
-      <!-- Compiled and minified JavaScript -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.0/js/materialize.min.js"></script>
-      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" media="all" rel="stylesheet" type="text/css" />
-      <link rel="stylesheet" href="./public/css/estilo.css">
-
-      <!-- Title -->
-      <title>FOXTROT - OS MELHORES SERVIÇOS</title>
-
-      <!-- Icon -->
-      <link rel="icon" href="./public/images/fox.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin</title>
 </head>
 <body>
+    <h1>ADMIN</h1>
+    
+    <?php 
+        echo $_SESSION['user'];
+    ?>
 
-    <section>
-        <article>
-            <div id="login">
-                <img id="img1" src="./public/images/2.jpg" alt="">
-                <div class="row">
-                    <div class="col s12 m6 offset-m3">
-                    <div id="cardLog" class="card white">
-                        <div id="title" class="card-action teal darken-1 z-depth-5">
-                            <div class="center">
-                                <img src="./public/images/fox.png" alt="">
-                            </div>
-                        </div>
-                        <div class="card-content white-text">
-                            <div class="row">
-                                <form class="col s12" method="post" action="./db/login.php">
-                                    <div class="row">
-                                    <div class="input-field col s10 offset-s1">
-                                        <input id="login_input" type="text" name="user" class="validate black-text text-darken-1">
-                                        <label for="login_input">Login</label>
-                                    </div>
-                                    <div class="input-field col s10 offset-s1">
-                                        <input id="pass" type="password" name="password" class="validate orange-text">
-                                        <label for="pass">Senha</label>
-                                    </div>
-                                    </div>
-                                    <br>
-                                    <div class="center">
-                                        <button type="submit" class="btn waves-effect waves-darken z-depth-3 orange darken-1" type="submit" name="action">entrar</button>
-                                    </div>
-                                </form>
-                            </div>                
-                        </div>
-                        <div class="center">
-                            <a href="#!" class="orange-text">Esqueci minha senha</a>
-                        </div>
-                    </div>
-                    </div>
-                </div>                
-            </div>
-        </article>
-    </section>
-
-
-  <!--Import jQuery before materialize.js-->
-  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-  <script src="http://github.hubspot.com/odometer/odometer.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
-  <!-- FadeIn clientes 1 -->
-  <script src="./public/javascript/init.js"></script>
+    <a href="../db/logout.php">Logout</a>
 </body>
 </html>
-
