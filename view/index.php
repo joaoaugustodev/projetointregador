7b94e8e4869df59e65109b94683cdd83b416ea37
João Luís <joaoluis@ibaro.com.br>
2017-10-23 17:10:49 -0200

    menu fixado

--- view/index.php
+++ view/index.php
@@ -25,20 +25,22 @@
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="white">
-        <div class="nav-wrapper container">
-          <a href="#" class="brand-logo">
-            <img src="../public/images/logo.png" alt="logotipo" width="60">
-          </a>
+    <div class="navbar-fixed">
+      <nav class="white">
+          <div class="nav-wrapper container">
+            <a href="./" class="brand-logo">
+              <img src="../public/images/logo.png" alt="logotipo" width="60">
+            </a>
 
-          <ul id="nav-mobile" class="right hide-on-med-and-down black-text">
-            <li><a class="black-text" href="#">Produtos</a></li>
-            <li><a class="black-text" href="#">Usuarios</a></li>
-            <li><a class="black-text" href="#">Pedidos</a></li>
-            <li><a class="logout z-depth-0 white-text btn" href="../db/logout.php">Logout</a></li>
-          </ul>
-        </div>
-    </nav>
+            <ul id="nav-mobile" class="right hide-on-med-and-down black-text">
+              <li><a class="black-text" href="#">Produtos</a></li>
+              <li><a class="black-text" href="#">Usuarios</a></li>
+              <li><a class="black-text" href="#">Pedidos</a></li>
+              <li><a class="logout z-depth-0 white-text btn" href="../db/logout.php">Logout</a></li>
+            </ul>
+          </div>
+      </nav>
+    </div>
     <br>
 
     <div class="container">
