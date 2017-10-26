0191b3416cb597cc0babd5a8ac360ac853433ab1
JulioCKlein <juliockleinc@gmail.com>
2017-10-26 21:44:44 -0200

    corrigido

---
+++ db/conection.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_host = "pi2s.database.windows.net";
+$db_name = "pi2";
+$db_user = "joaosenac2017";
+$db_pass = "02Jesuina";
+$dsn = "Driver={SQL Server};Server=$db_host;Port=1433;Database=$db_name;";
+
+if ( !$db = odbc_connect($dsn, $db_user, $db_pass)) {
+    echo "Erro ao conectar ao BANCO DE DADOS";
+    die();
+}
--- db/login.php
+++ db/login.php
@@ -1,5 +1,23 @@
 <?php
 
-require_once("./core/crud.class.php");
+include('./db/conection.php');
 
-echo $_REQUEST['user'];
+if (isset( $_REQUEST['user']) && isset($_REQUEST['password'])) {
+    session_start();
+    
+
+    $user = $_REQUEST['user'];
+    $pass = $_REQUEST['password'];
+
+
+    $stmt = odbc_prepare($db, 'SELECT idUsuario, nomeUsuario FROM Usuario WHERE loginUsuario = ? AND senhaUsuario = ?');
+    odbc_execute($stmt, array($user, $pass));
+    $usuario = odbc_fetch_array($stmt);
+    
+    if ($usuario) {
+       $_SESSION['user'] = $user;
+       header('Location: ./view/');
+    } else {
+        $msg = 'Login ou senha inválidos!';
+    }
+}
